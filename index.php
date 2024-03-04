190a5f5ff0bd036c85f339474ce956e025f48e0b
Luca10992 <lucalillo.99.1@gmail.com>
2024-03-04 15:02:32 +0100

    feat. print object

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+require_once __DIR__ . "./models/production.php";
 
+$titanic = new Production("Titanic", "Italiano", 8);
+$fightClub = new Production("Fight Club", "Italiano", 7);
+$zodiac = new Production("Zodiac", "Italiano", 9);
+$harryPotter = new Production("Harry Potter", "Inglese", 8);
+$FastandFurious = new Production("Fast and Furious", "Inglese", 6);
 
+var_dump($titanic, $fightClub, $zodiac, $harryPotter, $FastandFurious);
 ?>
 
 <!DOCTYPE html>
